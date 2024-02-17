29c888f15a3de12c5e213ed033e4b8e75ba2f8fb
Nandan Kandregula <nandan.kandregula@oracle.com>
2024-02-17 15:33:22 -0500

    removed tf version

--- infra/tf/client/management/main.tf
+++ infra/tf/client/management/main.tf
@@ -2,9 +2,6 @@
     // Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
     // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
     
-    terraform {
-      required_version = ">= 0.12.31"
-    }
     
     resource oci_core_vcn this {
       dns_label      = var.vcn_dns_label
